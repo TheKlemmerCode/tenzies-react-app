8f7470e06bad0e6092fb3ea73a6e95b326b85f52
Matthew Klemmer <theklemmercode@outlook.com>
2023-06-23 19:24:59 -0400

    Added rolling dice func, button styling, and nanoid for keys.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import { useEffect, useState } from 'react'
-import './assets/css/App.css'
+import {nanoid} from "nanoid"
+
 import Die from './assets/components/Die'
 
+import './assets/css/App.css'
+
 function App() {
+  const [dice, setDice] = useState(allNewDice())
+    
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push({
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+          })
+      }
+      return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice)
+  }
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
   
   return (
       <main>
         <div className='container'>
           <div className='dice-container'>
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
+            {diceElements}
           </div>
+          <button className='roll-button' onClick={rollDice}>Roll</button>
         </div>
       </main>
   )
